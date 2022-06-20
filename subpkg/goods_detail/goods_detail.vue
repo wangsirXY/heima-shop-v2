bb5b25bc41e5553a071a1e73859bdfa5577b8758
GuAn <15713912912@163.com>
2022-06-20 17:07:56 +0800

    feat::new:完成首页

---
+++ subpkg/goods_detail/goods_detail.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		商品详情页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				query: {}
+			};
+		},
+		onLoad(options) {
+			// 接收参数
+			this.query = options
+			// 动态设置当前商品标题
+			uni.setNavigationBarTitle({
+				title: options.goods_id
+			})
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ subpkg/goods_list/goods_list.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				query: {}
+			};
+		},
+		onLoad(options) {
+			// 接收参数
+			this.query = options
+			// 动态设置当前商品标题
+			uni.setNavigationBarTitle({
+				title: options.query
+			})
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
