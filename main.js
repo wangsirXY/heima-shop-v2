1831449869b86f7063b51cd82a505052037ef602
GuAn <15713912912@163.com>
2022-06-20 16:47:37 +0800

    chore::arrow_up:封装网络请求和错误信息提示

--- main.js
+++ main.js
@@ -2,8 +2,32 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
 
 Vue.config.productionTip = false
+
+uni.$http = $http
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+// 请求开始前
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+// 请求完成后
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
 
 App.mpType = 'app'
 
